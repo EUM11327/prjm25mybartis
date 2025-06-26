586118b22be719f7b764673e026b490da621550c
EUM11327 <gustjd11327@gmail.com>
2025-06-26 12:20:21 +0900

    hs1s1

--- src/main/java/com/tech/prjm09/controller/BController.java
+++ src/main/java/com/tech/prjm09/controller/BController.java
@@ -26,108 +26,107 @@ import oracle.jdbc.proxy.annotation.Post;
 
 @Controller
 public class BController {
-	//dev
+	// dev
 	BCommand command;
-	
+
 	private final IDao iDao;
-	
+
 	@Autowired
 	public BController(IDao iDao) {
-		this.iDao=iDao;
+		this.iDao = iDao;
 	}
-	
+
 	@RequestMapping("/list")
 	public String list(Model model) {
 		System.out.println("list() ctr");
 //		command=new BListCommand();
 //		command.execute(model);
-		ArrayList<BDto> list=iDao.list();
-		model.addAttribute("list",list);
-		
+		ArrayList<BDto> list = iDao.list();
+		model.addAttribute("list", list);
+
 		return "list";
 	}
 
 	@RequestMapping("/write_view")
 	public String write_view(Model model) {
 		System.out.println("write_view() ctr");
-		
+
 		return "write_view";
 	}
+
 	@RequestMapping("/write")
-	public String write(HttpServletRequest request,
-			Model model) {
+	public String write(HttpServletRequest request, Model model) {
 		System.out.println("write() ctr");
 //		db글쓰기동작
-		model.addAttribute("request",request);
-		command=new BWriteCommand();
-		command.execute(model);
-		
-		
+//		model.addAttribute("request",request);
+//		command=new BWriteCommand();
+//		command.execute(model);
+
+		String bname = request.getParameter("bname");
+		String btitle = request.getParameter("btitle");
+		String bcontent = request.getParameter("bcontent");
+		iDao.write(bname, btitle, bcontent);
 		return "redirect:list";
 	}
+
 	@RequestMapping("/content_view")
-	public String content_view(HttpServletRequest request,
-			Model model) {
+	public String content_view(HttpServletRequest request, Model model) {
 		System.out.println("content_view() ctr");
-		model.addAttribute("request",request);
-		command=new BContentCommand();
+		model.addAttribute("request", request);
+		command = new BContentCommand();
 		command.execute(model);
-		
+
 		return "content_view";
 	}
+
 	@RequestMapping("/modify_view")
-	public String modify_view(HttpServletRequest request,
-			Model model) {
+	public String modify_view(HttpServletRequest request, Model model) {
 		System.out.println("modify_view() ctr");
-		model.addAttribute("request",request);
-		command=new BModifyViewCommand();
+		model.addAttribute("request", request);
+		command = new BModifyViewCommand();
 		command.execute(model);
 		return "modify_view";
 	}
+
 	@PostMapping("/modify")
-	public String modify(HttpServletRequest request,
-			Model model) {
+	public String modify(HttpServletRequest request, Model model) {
 		System.out.println("modify() ctr");
-		model.addAttribute("request",request);
-		command=new BModifyCommand();
+		model.addAttribute("request", request);
+		command = new BModifyCommand();
 		command.execute(model);
-		
+
 		return "redirect:list";
 	}
+
 	@RequestMapping("/reply_view")
-	public String reply_view(HttpServletRequest request,
-			Model model) {
+	public String reply_view(HttpServletRequest request, Model model) {
 		System.out.println("reply_view() ctr");
-		
-		model.addAttribute("request",request);
-		command=new BReplyViewCommand();
+
+		model.addAttribute("request", request);
+		command = new BReplyViewCommand();
 		command.execute(model);
-		
+
 		return "reply_view";
 	}
+
 	@PostMapping("/reply")
-	public String reply(HttpServletRequest request,
-			Model model) {
+	public String reply(HttpServletRequest request, Model model) {
 		System.out.println("reply() ctr");
-		model.addAttribute("request",request);
-		command=new BReplyCommand();
+		model.addAttribute("request", request);
+		command = new BReplyCommand();
 		command.execute(model);
-		
+
 		return "redirect:list";
 	}
-	
+
 	@RequestMapping("/delete")
-	public String delete(HttpServletRequest request,
-			Model model) {
+	public String delete(HttpServletRequest request, Model model) {
 		System.out.println("delete() ctr");
-		model.addAttribute("request",request);
-		command=new BDeleteCommand();
+		model.addAttribute("request", request);
+		command = new BDeleteCommand();
 		command.execute(model);
-		
+
 		return "redirect:list";
 	}
-	
-	
-	
-	
+
 }
